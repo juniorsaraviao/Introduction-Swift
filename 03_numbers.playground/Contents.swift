82aed03f87fd7442d080fbb1274b94fe31a19512
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-16 11:45:37 -0500

    Use typealias

--- 03_numbers.playground/Contents.swift
+++ 03_numbers.playground/Contents.swift
@@ -24,3 +24,7 @@ let justMoreThanAMillion = 1_000_000.000_000_1
 let twoThousand: UInt16 = 2_000
 let one: UInt8 = 1
 let twoThousandPlusOne = twoThousand + UInt16(one)
+
+
+typealias AudioSample = UInt16
+var maxAmplitude = AudioSample.max
