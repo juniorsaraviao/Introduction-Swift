b5b9da97e8a2c8c0090d150d3f4058cafea3c17f
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-15 22:57:37 -0500

    Use symbol and print in swift

--- 02_var_let.playground/Contents.swift
+++ 02_var_let.playground/Contents.swift
@@ -10,3 +10,9 @@ var welcomeMessage : String
 welcomeMessage = "Hi, everyone"
 
 var red, gree, blue : Double
+
+let π = 3.141592
+
+print("The number of attemps is: \(currentLoginAttemp) out of \(maxnNumberOfAttemps)")
+
+print("Hello", "World", separator: ",", terminator: ".")
