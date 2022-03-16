88ccb58cf00be901524f0ad1a342cf405f30098a
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-16 13:43:59 -0500

    Use booleans

---
+++ 04_data_types.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let areOranger = true
+let foodIsDelicious = false
+
+var isAged: Bool
+isAged = false
+
+if isAged {
+    print("Enter the party!!!")
+}else {
+    print("Sorry, you can't enter the party")
+}
+
+var age = 31
+
+if age >= 18 {
+    print("Enter the party :)")
+}else {
+    print("Can't enter the party :(")
+}
