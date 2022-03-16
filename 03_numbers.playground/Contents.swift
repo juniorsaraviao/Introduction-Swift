7935c80058b1defd70280f7be1844294e9a946cf
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-16 11:15:52 -0500

    Use different numbers

--- 03_numbers.playground/Contents.swift
+++ 03_numbers.playground/Contents.swift
@@ -7,3 +7,16 @@ let maxValue = UInt8.max
 
 let f1 : Float = 3.14159265
 let d1 : Double = 3.14159265
+
+
+let decimalInteger = 17
+let binaryInteger = 0b10001
+let octalInteger = 0o21
+let hexaInteger = 0x11
+
+let decimalDouble = 12.1875
+let exponentDouble = 1.21875e1
+
+let paddedDouble = 123.456
+let oneMillion = 1_000_000
+let justMoreThanAMillion = 1_000_000.000_000_1
