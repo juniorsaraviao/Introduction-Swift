3e6bc7ccd12f30576b30049520ac207878849435
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-16 13:57:44 -0500

    Work with tuples

--- 04_data_types.playground/Contents.swift
+++ 04_data_types.playground/Contents.swift
@@ -19,3 +19,17 @@ if age >= 18 {
 }else {
     print("Can't enter the party :(")
 }
+
+
+//Tuples
+let http404Error = (404, "Page not found")
+let (statusCode, statusMessage) = http404Error
+print("Status code \(statusCode) - Message: \(statusMessage)")
+print(http404Error)
+
+let (justStatusCode, _) = http404Error
+print("Status code: \(justStatusCode)")
+print("Second way - Status code \(http404Error.0)")
+
+let http200Status = (statusCode: 200, description: "OK")
+print("Status code: \(http200Status.statusCode) - Description: \(http200Status.description)")
