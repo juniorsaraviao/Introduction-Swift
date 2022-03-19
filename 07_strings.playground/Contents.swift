3fe9938d8a185525cf3be0f7fe02b8e892ba5cc9
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-18 23:30:29 -0500

    Strings

---
+++ 07_strings.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let someString = "Sample string"
+
+let multiLineString = """
+    We're coding with swift
+    Greeting from Peru
+"""
+
+print(multiLineString)
+
+let wiseWords = "\"Imagination is more important than Knowledge\" - Albert Eistain"
+let dolarSig = "\u{24}"
+let blackHeart = "\u{2665}"
+let heart = "\u{1F480}"
+let flag = "\u{1F1F5}\u{1F1EA}"
