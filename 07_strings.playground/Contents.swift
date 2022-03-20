1fbe07b5feef849d4eda276ff7c3eb90e3c210d8
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-19 23:05:34 -0500

    use prefix and suffix

--- 07_strings.playground/Contents.swift
+++ 07_strings.playground/Contents.swift
@@ -59,3 +59,21 @@ let index = greeting.firstIndex(of: ",") ?? greeting.endIndex
 let firstPart = greeting[..<index] // type: substring
 
 let stringFirstPart = String(firstPart) // type: string
+
+let newGreeting = "Hi, welcome"
+newGreeting.hasPrefix("Hi")
+newGreeting.hasSuffix("bye")
+
+
+let collection = [
+    "Act 1 Scene 1", "Act 1 Scene 2", "Act 1 Scene 3", "Act 1 Scene 4",
+    "Act 2 Scene 1", "Act 2 Scene 2", "Act 2 Scene 3",
+    "Act 3 Scene 1", "Act 3 Scene 3"
+]
+var act1SceneCount = 0
+for scene in collection {
+    if scene.hasPrefix("Act 1") {
+        act1SceneCount += 1
+    }
+}
+print(act1SceneCount)
