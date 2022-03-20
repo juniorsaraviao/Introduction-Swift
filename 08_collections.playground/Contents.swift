a88bc51e4929ffd5236f29dae278253c05fac0bc
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-20 17:03:17 -0500

    modify arrays

--- 08_collections.playground/Contents.swift
+++ 08_collections.playground/Contents.swift
@@ -11,3 +11,20 @@ var someDoubles = Array(repeating: 3.141592, count: 5)
 someDoubles.count
 
 var moreDouble = Array(repeating: 2.5, count: 2) + someDoubles
+
+var shoppingList = ["Potatoes", "Beans", "Pepper", "Lettuce"]
+shoppingList.count
+
+if shoppingList.isEmpty {
+    print("The shopping list is empty")
+}else {
+    print("It's ready to order")
+}
+
+shoppingList.append("Soda")
+shoppingList.count
+
+var firstElement = shoppingList.first
+shoppingList[1...3]
+shoppingList[1...3] = ["Bananas", "Oranges", "Apples"]
+shoppingList
