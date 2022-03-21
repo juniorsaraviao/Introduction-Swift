0ef707e22d631e1a64f3b3611f82ae62212381c6
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-20 21:20:50 -0500

    Use fallthrough

--- 09_control_flows.playground/Contents.swift
+++ 09_control_flows.playground/Contents.swift
@@ -77,3 +77,15 @@ for ch in sentence {
     }
 }
 print(filteredSentence)
+
+let integerToDescribe = 5
+var description = "The number \(integerToDescribe) is"
+
+switch integerToDescribe {
+case 2,3,5,11,13,17,19:
+    description += " prime and"
+    fallthrough // evaluates the default case too ✅
+default:
+    description += " an integer"
+}
+print(description)
