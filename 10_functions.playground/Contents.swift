a67fb828a11e0ea7d8ffe60cfa39f60521f6821e
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-20 22:28:06 -0500

    alias and default

--- 10_functions.playground/Contents.swift
+++ 10_functions.playground/Contents.swift
@@ -35,3 +35,20 @@ let bounds = minMax(array: [6, 3,-8,3,1,9,5,15,-9])
 print("The values are from \(bounds!.min) and \(bounds!.max)")
 minMax(array: [])
 
+// alias and default
+func greetings(_ person: String, from hometown: String = "USA") -> String {
+    return "Hi, \(person) from \(hometown)"
+}
+
+greetings("Random", from: "Peru")
+greetings("Random 2")
+
+
+func mean(_ numbers: Double...) -> Double {
+    var total: Double = 0;
+    for number in numbers {
+        total += number
+    }
+    return total / Double(numbers.count)
+}
+mean(5,4,6,7,1)
