9c77880430fdf4020e5203d6f21fae58ea41cb81
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-20 19:01:59 -0500

    while loop

--- 09_control_flows.playground/Contents.swift
+++ 09_control_flows.playground/Contents.swift
@@ -10,3 +10,16 @@ for name in names {
 for tickMark in stride(from: 0, to: 60, by: 5) {
     print(String(format: "%02d", tickMark))
 }
+
+
+// while
+var i = 0
+while i <= 10 {
+    print(i)
+    i += 1
+}
+
+repeat {
+    i += 1
+} while i < 10
+print(i)
