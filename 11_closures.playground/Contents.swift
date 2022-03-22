222d393fcfa16566854071abc098bcba5def02f5
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-21 23:20:43 -0500

    trailing closures

--- 11_closures.playground/Contents.swift
+++ 11_closures.playground/Contents.swift
@@ -18,3 +18,35 @@ print(reverseName)
 // Other ways:
 //reverseName = names.sorted(by: { $0 > $1})
 //reverseName = names.sorted(by: >)
+
+
+func someFunctionThatTakesAClosure(closure: () -> Void) {
+    // Code here!
+}
+
+someFunctionThatTakesAClosure(closure: {
+    // Code here!
+})
+
+someFunctionThatTakesAClosure {
+    <#code#>
+}
+
+let digits = [0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five",
+              6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"]
+let numbers = [-16, 58, 2127]
+
+let numberStrings = numbers.map { (number) -> String in
+    
+    func change(number: Int) -> String {
+        var number = number
+        var output = ""
+        repeat {
+            output = digits[number % 10]! + output
+            number /= 10
+        }while number > 0
+        return output
+    }
+    
+    return number < 0 ? "minus" + change(number: number * -1) : change(number: number)
+}
