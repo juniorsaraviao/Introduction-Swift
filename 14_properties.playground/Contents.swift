99c26a0174b993417e9f6f059df12c66e2c5904f
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-23 23:16:51 -0500

    stored properties

---
+++ 14_properties.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+struct FixedLengthRange {
+    var firstValue: Int
+    let length: Int
+}
+
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+rangeOfThreeItems.firstValue = 6
+let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
+//rangeOfFourItems.firstValue = 7 -> error
