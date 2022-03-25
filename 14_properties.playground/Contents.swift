70660498c2556eedcb5fbb269ae2689badc2a0bc
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-24 23:20:44 -0500

    lazy properties

--- 14_properties.playground/Contents.swift
+++ 14_properties.playground/Contents.swift
@@ -9,3 +9,20 @@ var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
 rangeOfThreeItems.firstValue = 6
 let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
 //rangeOfFourItems.firstValue = 7 -> error
+
+class DataImporter {
+    var filename = "data.txt"
+}
+
+
+class DataManager {
+    lazy var importer = DataImporter()
+    var data = [String]()
+}
+
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("more data")
+manager
+// the importer is created just below (not before)
+manager.importer.filename
