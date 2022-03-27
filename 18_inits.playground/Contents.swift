845d90c51294851a510cc427933e81aa8cca3667
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-27 18:40:20 -0500

    optionals and inits

--- 18_inits.playground/Contents.swift
+++ 18_inits.playground/Contents.swift
@@ -24,3 +24,42 @@ struct Celsius {
 
 let boilingPointOfWater = Celsius(fromFahrenheit: 212)
 let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+
+
+struct Color {
+    let red, green, blue: Double
+    
+    init(red: Double, green: Double, blue: Double) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+    }
+    
+    init(white: Double) {
+        self.red = white
+        self.green = white
+        self.blue = white
+    }
+}
+
+let magenta = Color(red: 1, green: 0, blue: 1)
+let halfGray = Color(white: 0.5)
+let green = Color(red: 0, green: 1, blue: 0)
+
+
+class SuverQuestion {
+    let text: String
+    var response: String?
+    
+    init(text: String) {
+        self.text = text
+    }
+    
+    func ask() {
+        print(text)
+    }
+}
+
+let q1 = SuverQuestion(text: "Do you like tacos?")
+q1.ask()
+q1.response = "Yes, I do"
