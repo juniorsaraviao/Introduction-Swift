ba63ace7503e22d68ad196445ba36ff2fca214fe
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-27 19:23:53 -0500

    use of deinit

--- 18_inits.playground/Contents.swift
+++ 18_inits.playground/Contents.swift
@@ -145,3 +145,44 @@ class CartItem: Product {
 if let someSocks = CartItem(name: "Socks", quantity: 2) {
     print("\(someSocks.name) - \(someSocks.quantity)")
 }
+
+
+class Bank {
+    static var coinsInBank = 2_000
+    
+    static func distribute(coins numberOfCoinsRequested: Int) -> Int {
+        let numberOfCoinsToVend = min(numberOfCoinsRequested, coinsInBank)
+        coinsInBank -= numberOfCoinsToVend
+        return numberOfCoinsToVend
+    }
+    
+    static func receive(coins: Int) {
+        coinsInBank += coins
+    }
+}
+
+class Player {
+    var coinsInPurse: Int
+    
+    init(coins: Int) {
+        self.coinsInPurse = Bank.distribute(coins: coins)
+    }
+    
+    func win(coins: Int) {
+        coinsInPurse += Bank.distribute(coins: coins)
+    }
+    
+    deinit {
+        Bank.receive(coins: coinsInPurse)
+    }
+}
+
+
+var playerOne: Player? = Player(coins: 100)
+Bank.coinsInBank
+playerOne!.win(coins: 2_000)
+playerOne
+Bank.coinsInBank
+
+playerOne = nil
+Bank.coinsInBank
